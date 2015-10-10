83bb7b6bd973f43c52d0c9a789bacbee3d0c8096
haomeng <haomeng1@yeah.net>
2015-10-10 19:11:46 +0800

    Add getMed()

--- src/mpi_bcast.cpp
+++ src/mpi_bcast.cpp
@@ -8,12 +8,21 @@
 #include <iomanip>
 using namespace std;
 
-void data_init(int * send_or_recv, int size)
+double getMed(double values[], int size)
 {
-   for(int i = 0;i< size;++i)
-   {
-      send_or_recv[i] = rand(); 
-   }
+   int x,y;
+   double handle;
+
+   for(x = 0; x < size; ++x)
+      for(y = 0; y < size; ++y)
+         if(values[x] > values[y])
+         {
+            handle = values[x];
+            values[x] = values[y];
+            values[y] = handle;
+         }
+
+   return values[(size+1)/2];
 }
 int main(int argc ,char* argv[])
 {
@@ -29,26 +38,30 @@ int main(int argc ,char* argv[])
    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
    int parNum = atoi(argv[1]);
    int size = atoi(argv[2]);
-   srand((unsigned)time(NULL));
    //MPI_Barrier(MPI_COMM_WORLD);
 
-   double total_mpi_bcast_time = 0.0;
+
+   double values[parNum];
+
    for(int i = 0;i < parNum; ++i)
    {
-      int* data = new int[size];
-      data_init(data,size);
+      double total_mpi_bcast_time = 0.0;
+      char* data = new char[size];
 
       MPI_Barrier(MPI_COMM_WORLD);
       total_mpi_bcast_time -= MPI_Wtime();
-      MPI_Bcast(data, size,MPI_INT,0,MPI_COMM_WORLD);
+      MPI_Bcast(data, size,MPI_CHAR,0,MPI_COMM_WORLD);
       MPI_Barrier(MPI_COMM_WORLD);
       total_mpi_bcast_time += MPI_Wtime();
+
+      values[i] = total_mpi_bcast_time;
+
 
       delete[] data;
    }
    if(my_rank == 0)
    {
-      cout << size * sizeof(int) << "\t" << fixed << setprecision(15) << total_mpi_bcast_time/parNum << "\n";  
+      cout << size * sizeof(char) << "\t" << fixed << setprecision(15) << getMed(values, parNum) << "\n";  
    }
    MPI_Finalize();
    return 0;
